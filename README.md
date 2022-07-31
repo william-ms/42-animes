c4c6e2e68148752bbe10d0f947f659e633c30a28
William-MS <williamsartinidev@outlook.com>
2022-07-31 00:35:44 -0300

    função menu mobile

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Esse é meu primeiro projeto que desenvolvi do zero usando meu conhecimento de H
 A ideia central do foi produzir um site com informações nerds sobre animes, mangás, desenhos, filmes e etc.
 
 Nesse protótipo eu usei o anime yu-gi-oh! para desenvolver a parte com os decks de cartas dos personagens.
+
+
+### Commits
+
+01. Montei todo o html e css do protótipo.
+
+02. Desenvolvi a função para atualizar o contéudo da página usando Ajax.
+
+03. Adicionei a função de rolagem para o menu mobile.
--- js/fun-page.js
+++ js/fun-page.js
@@ -1,29 +1,48 @@
-var content = document.getElementById('content');
-var url = document.querySelectorAll('.list p');
+/****************************************************************************************************************/
+/********************************************** AJAX CONTEUDO ***************************************************/
 
-var xhr;
-if (window.XMLHttpRequest) {
-    xhr = new XMLHttpRequest();
-} else if (window.ActiveXObject) {
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+function atualizarConteudo() {
+    var content = document.getElementById('content');
+    var url = document.querySelectorAll('.list p');
+
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    for(var i = 0; i < url.length; i++){
+        let urlAtual = url[i].getAttribute('value');
+
+        url[i].addEventListener('click', function(){
+            xhr.open('GET', urlAtual);
+
+            xhr.send();
+        
+            xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                content.innerHTML = xhr.response;
+            }}
+        })
+    }
 }
 
-for(var i = 0; i < url.length; i++){
-    let urlAtual = url[i].getAttribute('value');
+atualizarConteudo();
 
-    url[i].addEventListener('click', function(){
-        xhr.open('GET', urlAtual);
+/****************************************************************************************************************/
+/*********************************************** MENU MOBILE ****************************************************/
 
-        xhr.send();
-    
-        xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200){
-            content.innerHTML = xhr.response;
-        }}
-    })
-    
+function toggleMenu(){
+
+    const menuMobile = document.getElementById('menu-mobile');
+
+    menuMobile.addEventListener('click', function(){
+        menuMobile.classList.toggle('menu-active');
+    });
 }
 
+toggleMenu();
 
-
-
+/****************************************************************************************************************/
+/********************************************** HEADER FIXADO ***************************************************/
